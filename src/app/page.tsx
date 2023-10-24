23dafe8fedb96b5e739cfaf26ac1d097c0f6ef05
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-10-24 21:54:09 +0800

    sub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,15 @@ function App() {
         <DynamicComponent />
         <div className="landing-container">
           <div className="landing">
-            <h1 className="main-title"> ERCKNARD GUREN V </h1>
+            <div className="main-title"> ERCKNARD GUREN V </div>
+            <div className="sub-title to-width">
+              {" "}
+              Ercknard web portfolio is a digital exhibit of my creative
+              journey. Explore my projects, designs, and achievements all in one
+              place. Get to know my skills and passion through a collection of
+              my best work, beautifully presented for your inspiration and
+              insight.{" "}
+            </div>
           </div>
         </div>
       </main>
