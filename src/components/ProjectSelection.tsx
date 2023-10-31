678b73fa7036e170a1cccb95222951b8950aa1ff
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-10-31 21:56:55 +0800

    hi

--- src/components/ProjectSelection.tsx
+++ src/components/ProjectSelection.tsx
@@ -12,63 +12,89 @@ export default class ProjectSelection extends Component<Props, State> {
     return (
       <>
         <section>
-          <div className="to-flex project-section">
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/erckportfolio.jpeg"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/aercrypt.jpeg"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/cryptechservices.PNG"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/metrixplace.jpeg"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/cryptechtest.PNG"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/cryptechcraft.PNG"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/minecraftgallery.PNG"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
-            <Image
-              className="project-selection-image"
-              src="/static/images/projects/maysilo.PNG"
-              alt="slide_image"
-              width={1325}
-              height={851}
-            />
+          <div className="to-gap-project">
+            <div className="to-flex project-section">
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/erckportfolio.jpeg"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/aercrypt.jpeg"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/cryptechservices.PNG"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/metrixplace.jpeg"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/cryptechtest.PNG"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/cryptechcraft.PNG"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/minecraftgallery.PNG"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+              <div className="project-portrait">
+                <span className="project-logo" />
+                <Image
+                  className="project-selection-image"
+                  src="/static/images/projects/maysilo.PNG"
+                  alt="slide_image"
+                  width={1325}
+                  height={851}
+                />
+              </div>
+            </div>
           </div>
         </section>
       </>
