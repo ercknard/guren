22aefec525c810e53d4ee82b528f3fdf039d35f9
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-10-31 23:23:02 +0800

    s

--- src/components/ProjectSelection.tsx
+++ src/components/ProjectSelection.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 type Props = {};
 
@@ -11,14 +13,25 @@ export default class ProjectSelection extends Component<Props, State> {
   render() {
     return (
       <>
-        <section>
+        <section data-aos="fade-up" data-aos-duration="1200">
           <div className="to-gap-project">
             <div className="to-flex project-section">
               <div className="project-portrait">
                 <span className="project-logo" />
                 <span className="project-description-bg" />
                 <span className="project-description">
-                  Ercknard Web Portfolio
+                  <span className="text-effect project-name">
+                    Ercknard Web Portfolio
+                  </span>
+                  <Link
+                    href="/projects/ercknard-web-portfolio"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
                 </span>
                 <Image
                   className="project-selection-image"
@@ -31,7 +44,15 @@ export default class ProjectSelection extends Component<Props, State> {
               <div className="project-portrait">
                 <span className="project-logo" />
                 <span className="project-description-bg" />
-                <span className="project-description">Aercrypt.net</span>
+                <span className="project-description">
+                  <span className="text-effect project-name">Aercrypt.net</span>
+                  <Link href="/projects/aercynet" rel="noopener noreferrer">
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
+                </span>
                 <Image
                   className="project-selection-image"
                   src="/static/images/projects/aercrypt.webp"
@@ -43,7 +64,20 @@ export default class ProjectSelection extends Component<Props, State> {
               <div className="project-portrait">
                 <span className="project-logo" />
                 <span className="project-description-bg" />
-                <span className="project-description">Cryptech Services</span>
+                <span className="project-description">
+                  <span className="text-effect project-name">
+                    Cryptech Services
+                  </span>
+                  <Link
+                    href="/projects/cryptech-services"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
+                </span>
                 <Image
                   className="project-selection-image"
                   src="/static/images/projects/cryptechservices.webp"
@@ -55,7 +89,15 @@ export default class ProjectSelection extends Component<Props, State> {
               <div className="project-portrait">
                 <span className="project-logo" />
                 <span className="project-description-bg" />
-                <span className="project-description">Metrix.place</span>
+                <span className="project-description">
+                  <span className="text-effect project-name">Metrix.place</span>
+                  <Link href="/projects/metrix-place" rel="noopener noreferrer">
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
+                </span>
                 <Image
                   className="project-selection-image"
                   src="/static/images/projects/metrixplace.webp"
@@ -68,7 +110,18 @@ export default class ProjectSelection extends Component<Props, State> {
                 <span className="project-logo" />
                 <span className="project-description-bg" />
                 <span className="project-description">
-                  CryptechTest Website
+                  <span className="text-effect project-name">
+                    CryptechTest Website
+                  </span>
+                  <Link
+                    href="/projects/cryptechtest-website"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
                 </span>
                 <Image
                   className="project-selection-image"
@@ -81,7 +134,20 @@ export default class ProjectSelection extends Component<Props, State> {
               <div className="project-portrait">
                 <span className="project-logo" />
                 <span className="project-description-bg" />
-                <span className="project-description">CryptectCraft</span>
+                <span className="project-description">
+                  <span className="text-effect project-name">
+                    CryptectCraft
+                  </span>
+                  <Link
+                    href="/projects/cryptechcraft"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
+                </span>
                 <Image
                   className="project-selection-image"
                   src="/static/images/projects/cryptechcraft.webp"
@@ -94,7 +160,18 @@ export default class ProjectSelection extends Component<Props, State> {
                 <span className="project-logo" />
                 <span className="project-description-bg" />
                 <span className="project-description">
-                  Ercknard Minecraft Gallery
+                  <span className="text-effect project-name">
+                    Ercknard Minecraft Gallery
+                  </span>
+                  <Link
+                    href="/projects/ercknard-minecraft-gallery"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
                 </span>
                 <Image
                   className="project-selection-image"
@@ -107,7 +184,20 @@ export default class ProjectSelection extends Component<Props, State> {
               <div className="project-portrait">
                 <span className="project-logo" />
                 <span className="project-description-bg" />
-                <span className="project-description">Maysilo Meter</span>
+                <span className="project-description">
+                  <span className="text-effect project-name">
+                    Maysilo Meter
+                  </span>
+                  <Link
+                    href="/projects/maysilo-meter"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="read-more">
+                      <OpenInNewIcon className="to-color" />{" "}
+                      <span>Read more</span>
+                    </div>
+                  </Link>
+                </span>
                 <Image
                   className="project-selection-image"
                   src="/static/images/projects/maysilo.webp"
