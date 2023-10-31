b97cfeae5b913d16d4eeb74e942d53aff3711dd2
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-10-31 12:12:34 +0800

    Update index.tsx

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -8,6 +8,10 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import DiscFullIcon from "@mui/icons-material/DiscFull";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import DeviceHubIcon from "@mui/icons-material/DeviceHub";
+import Link from "next/link";
 import Image from "next/image";
 
 type Props = {};
@@ -48,26 +52,76 @@ export default class Contact extends Component<Props, State> {
                   data-aos-duration="3000"
                   className="to-flex socials"
                 >
-                  <div className="social-icons">
-                    <FacebookIcon className="to-color" />{" "}
-                    <span className="to-hide  social-links">Facebook</span>
-                  </div>
-                  <div className="social-icons">
-                    <TwitterIcon className="to-color" />{" "}
-                    <span className="to-hide  social-links">Twitter</span>
-                  </div>
-                  <div className="social-icons">
-                    <DiscFullIcon className="to-color" />{" "}
-                    <span className="to-hide social-links">Discord</span>
-                  </div>
-                  <div className="social-icons">
-                    <AlternateEmailIcon className="to-color" />{" "}
-                    <span className="to-hide  social-links">Email</span>
-                  </div>
-                  <div className="social-icons">
-                    <GitHubIcon className="to-color" />{" "}
-                    <span className="to-hide  social-links">Github</span>
-                  </div>
+                  <Link
+                    target="_blank"
+                    href="https://fb.com"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="social-icons">
+                      <FacebookIcon className="to-color" />{" "}
+                      <span className="to-hide  social-links">Facebook</span>
+                    </div>
+                  </Link>
+                  <Link
+                    target="_blank"
+                    href="https://twitter.com"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="social-icons">
+                      <TwitterIcon className="to-color" />{" "}
+                      <span className="to-hide  social-links">Twitter</span>
+                    </div>
+                  </Link>
+                  <Link
+                    target="_blank"
+                    href="https://discord.com"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="social-icons">
+                      <MarkEmailReadIcon className="to-color" />{" "}
+                      <span className="to-hide social-links">Discord</span>
+                    </div>
+                  </Link>
+                  <Link
+                    target="_blank"
+                    href="https://github.com"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="social-icons">
+                      <GitHubIcon className="to-color" />{" "}
+                      <span className="to-hide  social-links">Github</span>
+                    </div>
+                  </Link>
+                  <Link
+                    target="_blank"
+                    href="https://linkedin.com"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="social-icons">
+                      <LinkedInIcon className="to-color" />{" "}
+                      <span className="to-hide social-links">Linkedin</span>
+                    </div>
+                  </Link>
+                  <Link
+                    target="_blank"
+                    href="https://indeed.com"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="social-icons">
+                      <DeviceHubIcon className="to-color" />{" "}
+                      <span className="to-hide social-links">Indeed</span>
+                    </div>
+                  </Link>
+                  <Link
+                    target="_blank"
+                    href="mailto:email@example.com"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="social-icons">
+                      <AlternateEmailIcon className="to-color" />{" "}
+                      <span className="to-hide  social-links">Email</span>
+                    </div>
+                  </Link>
                 </div>
                 <div className="normal-text">
                   <span data-aos="fade-up" data-aos-duration="1200">
