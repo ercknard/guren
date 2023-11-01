7c680bdac2fd02992ec0e4efc41b417632a258f7
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-11-01 19:49:55 +0800

    about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import Head from "next/head";
 import Layout from "@/layout/Layout";
 import TypingAnimation from "@/components/AboutTextAnimation";
+import Link from "next/link";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import DeviceHubIcon from "@mui/icons-material/DeviceHub";
+import Image from "next/image";
 import AboutCoverflow from "@/components/AboutCoverflow";
 import PageSigns from "@/components/PageSigns";
 import CryptechTeam from "@/components/CryptechTeam";
@@ -26,25 +37,133 @@ export default class About extends Component<Props, State> {
             <div className="pages-banner" />
             <div className="to-big">
               <div className="content-pages">
-                {/* <div className="to-flex">
-                  <div className="">
-                    <div className="">___ HELLO,</div>
-                    <div className="">I&apos;m Ercknard.</div>
-                    <div className="">
+                <div className="to-flex to-align main-about">
+                  <div className="to-about-with">
+                    <div className="greeting-main-name">---HELLO</div>
+                    <div className="about-main-name text-effect">
+                      I&apos;m ERCKNARD.
+                    </div>
+                    <div className="animated-text">
                       {" "}
                       <TypingAnimation />
-                      Web Designer | Developer | Graphic Designer
                     </div>
-                    <div className="">
+                    <div className="about-sub-text">
                       {" "}
-                      I am a versatile creative professional with a passion for
-                      web designing, web development, and graphic design. With a
+                      This is <span className="spies">Ercknard Guren</span>,
+                      versatile creative professional with a passion for web
+                      designing, web development, and graphic design. With a
                       keen eye for aesthetics and a knack for turning ideas into
                       stunning digital experiences, I bring innovation and flair
                       to every project I undertake.
                     </div>
+                    <div
+                      data-aos="fade"
+                      data-aos-duration="3000"
+                      className="to-flex socials"
+                    >
+                      <Link
+                        target="_blank"
+                        href="https://fb.com"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social-icons">
+                          <FacebookIcon className="to-color" />{" "}
+                          <span className="to-hide  social-links">
+                            Facebook
+                          </span>
+                        </div>
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href="https://twitter.com"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social-icons">
+                          <TwitterIcon className="to-color" />{" "}
+                          <span className="to-hide  social-links">Twitter</span>
+                        </div>
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href="https://discord.com"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social-icons">
+                          <MarkEmailReadIcon className="to-color" />{" "}
+                          <span className="to-hide social-links">Discord</span>
+                        </div>
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href="https://github.com"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social-icons">
+                          <GitHubIcon className="to-color" />{" "}
+                          <span className="to-hide  social-links">Github</span>
+                        </div>
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href="https://linkedin.com"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social-icons">
+                          <LinkedInIcon className="to-color" />{" "}
+                          <span className="to-hide social-links">Linkedin</span>
+                        </div>
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href="https://indeed.com"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social-icons">
+                          <DeviceHubIcon className="to-color" />{" "}
+                          <span className="to-hide social-links">Indeed</span>
+                        </div>
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href="mailto:email@example.com"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social-icons">
+                          <AlternateEmailIcon className="to-color" />{" "}
+                          <span className="to-hide  social-links">Email</span>
+                        </div>
+                      </Link>
+                    </div>
+                    <div className="to-flex teams-end-btn to-gap">
+                      <Link
+                        target="_blank"
+                        href="https://cryptech.services/"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="project-button">
+                          <OpenInNewIcon className="to-color send-icon" />{" "}
+                          <span> Cryptech site</span>
+                        </div>
+                      </Link>
+                      <Link
+                        href="/projects/cryptech-services"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="project-button">
+                          <FileDownloadIcon className="to-color send-icon" />{" "}
+                          <span> Download CV</span>
+                        </div>
+                      </Link>
+                    </div>
                   </div>
-                </div> */}
+                  <div className="main-avatar">
+                    <div
+                      data-aos="fade"
+                      data-aos-duration="1500"
+                      className="main-head-profile"
+                    />
+                  </div>
+                </div>
                 {/* <div
                   data-aos="fade-down"
                   data-aos-duration="1200"
@@ -63,7 +182,7 @@ export default class About extends Component<Props, State> {
                   </span>
                   <AboutCoverflow />
                 </div> */}
-                <CryptechTeam />
+                {/* <CryptechTeam /> */}
               </div>
             </div>
           </section>
--- src/components/AboutTextAnimation.tsx
+++ src/components/AboutTextAnimation.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 const TypingAnimation: React.FC = () => {
-  const textToType = "This is a typing animation!";
+  const textToTypes = ["Web Designer.", "Developer.", "Graphic Designer."];
+
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
   const [typedText, setTypedText] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -9,11 +11,21 @@ const TypingAnimation: React.FC = () => {
     const typingSpeed = 100; // Adjust the typing speed in milliseconds
 
     const typeNextCharacter = () => {
-      if (currentIndex < textToType.length) {
+      if (currentIndex < textToTypes[currentTextIndex].length) {
         setTypedText(
-          (prevTypedText) => prevTypedText + textToType[currentIndex]
+          (prevTypedText) =>
+            prevTypedText + textToTypes[currentTextIndex][currentIndex]
         );
         setCurrentIndex((prevIndex) => prevIndex + 1);
+      } else {
+        // Wait for a moment, then clear the text and move to the next sentence
+        setTimeout(() => {
+          setTypedText("");
+          setCurrentIndex(0);
+          setCurrentTextIndex(
+            (prevTextIndex) => (prevTextIndex + 1) % textToTypes.length
+          );
+        }, 3000); // You can adjust the delay before moving to the next sentence
       }
     };
 
@@ -22,9 +34,15 @@ const TypingAnimation: React.FC = () => {
     return () => {
       clearInterval(typingInterval);
     };
-  }, [currentIndex]);
+  }, [currentIndex, currentTextIndex]);
 
-  return <div>{typedText}</div>;
+  return (
+    <div>
+      {" "}
+      Aspiring <span className="text-effect"> {typedText} </span>{" "}
+      <span className="to-blink">|</span>
+    </div>
+  );
 };
 
 export default TypingAnimation;
