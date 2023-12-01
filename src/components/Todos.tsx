fbd5cc96cd29ac4c678f58516fb97a9453672e6d
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-12-02 00:42:57 +0800

    Update Todos.tsx

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,15 +1,6 @@
 import * as React from "react";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
-import TimelineDot from "@mui/lab/TimelineDot";
 import Typography from "@mui/material/Typography";
 import PageSigns from "./PageSigns";
-import CheckIcon from "@mui/icons-material/Check";
-import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 
 export default function Todos() {
   return (
