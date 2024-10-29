7a26dfffd9da56d6453a1b23945e9886f871238b
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2024-10-29 16:00:02 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {" "}
-        <StarsCanvas />
+        {/* <StarsCanvas /> */}
         {children}
       </body>
       <AOScall />
